2be8a9e6b45422ed609afe02a4cfa400d7b86f6d
Benjamin Baka <baka@mpedigree.net>
2021-06-20 05:18:57 +0000

    Ben: allow script to set and delete db file

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-
+import os, sys
 engine = create_engine('sqlite:////tmp/test.db')
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
@@ -13,6 +13,14 @@ def init_db():
     # import all modules here that might define models so that
     # they will be registered properly on the metadata.  Otherwise
     # you will have to import them first before calling init_db()
+    try:
+        action = sys.argv[1]
+    except:
+        action = ""
+    if action =="reset":
+        if os.path.exists("/tmp/test.db"):
+            os.remove("/tmp/test.db")
+
     import models
     Base.metadata.create_all(bind=engine)
 
